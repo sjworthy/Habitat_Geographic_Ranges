2de61cfb6189084d81887296e2e4a74d453ebbc7
Samantha Worthy <sjworthy@ucdavis.edu>
2025-02-23 21:49:07 -0600

    getting HCC going

---
+++ Scripts/split.DEMs.R
@@ -0,0 +1,20 @@
+## Script to split the DEMs in to 6 parts
+
+library(topoclimate.pred)
+library(SpaDES)
+
+setwd("/Volumes/My Passport for Mac/DEMS")
+
+# Read in the raster files as a list
+raster_files <- list.files(".", pattern = "*.tif", full.names = TRUE)
+
+for(raster_file in raster_files){
+  
+  # Load the DEM
+  elev <- raster(raster_file)
+  names(elev) = str_remove(basename(elev@file@name), "\\.tif$")
+  splitRaster(elev, nx = 3, ny = 3, path = "./split.rasters/")
+}
+
+
+
