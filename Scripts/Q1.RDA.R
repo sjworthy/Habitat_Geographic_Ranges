5623e9717e6cc26551439c1c9ff85542fe3d347e
Samantha Worthy <sjworthy@ucdavis.edu>
2025-03-13 22:27:32 -0500

    trying to knit the file, but too big

--- Scripts/Q1.RDA.R
+++ Scripts/Q1.RDA.R
@@ -11,10 +11,20 @@ library(tidyverse)
 
 #### RDA ####
 
-species.list = list.files(".", pattern = "*.csv", full.names = TRUE)
+# significance testing using anova.cca, automatically does 999 permutations.
+# F statistic corresponds to an overall test of sign. of an RDA by comparing the computed model to
+# a null model. This test is based on the null hypothesis that the strength of the linear relationship
+# calculated by the R2 is not larger than the value that would be obtained for unrelated Y and X matrices
+# of the same size. 
+
+# constrained proportion: variance of Y explained by X
+# unconstrained proportion: unexplained variance in Y
+
+species.list = list.files("./Results/Species.Climate/", pattern = "*.csv", full.names = TRUE)
 
 results <- data.frame(
   species = character(),
+  n = numeric(),
   full_r2 = numeric(),
   full_constrained_var = numeric(),
   full_unconstrained_var = numeric(),
@@ -26,6 +36,8 @@ results <- data.frame(
   long_constrained_var = numeric(),
   long_unconstrained_var = numeric(),
   full_anova_p_value = numeric(),
+  full_anova_lat_p_value = numeric(),
+  full_anova_long_p_value = numeric(),
   lat_anova_p_value = numeric(),
   long_anova_p_value = numeric(),
   stringsAsFactors = FALSE
@@ -36,6 +48,8 @@ for(species_file in species.list){
   # Read in the species data
   species = read.csv(species_file)
   
+  number.individuals = nrow(species)
+  
   # create response matrix, scaling variables b/c different units
   response = species %>%
     select(high_temp_C,low_temp_C,moisture_mm) %>%
@@ -45,13 +59,15 @@ for(species_file in species.list){
   full.rda = rda(response ~ decimalLatitude + decimalLongitude, species)
   full_r2 <- RsquareAdj(full.rda)$adj.r.squared
   full.rda.summary = summary(full.rda)
-  full.constrained.var = full.rda.summary$cont$importance[2, ]
-  full.unconstrained.var = full.rda.summary$cont$importance[3, ]
+  full.constrained.var = full.rda.summary$constr.chi/full.rda.summary$tot.chi
+  full.unconstrained.var = full.rda.summary$unconst.chi/full.rda.summary$tot.chi
   
   # significance of rda
   full.anova = anova.cca(full.rda)
-  full_anova_p_value <- full_anova$`Pr(>F)`[1]
-###########  full.anova.terms = anova.cca(full.rda, by = "terms")
+  full_anova_p_value <- full.anova$`Pr(>F)`[1]
+  full.anova.terms = anova.cca(full.rda, by = "terms")
+  full_anova_lat_p_value = full.anova.terms$`Pr(>F)`[1]
+  full_anova_long_p_value = full.anova.terms$`Pr(>F)`[2]
   
   # get correlation between lat and long for the species
   geo.cor = cor(species$decimalLatitude, species$decimalLongitude)
@@ -60,40 +76,44 @@ for(species_file in species.list){
   lat.rda = rda(response ~ decimalLatitude, species)
   lat_r2 <- RsquareAdj(lat.rda)$adj.r.squared
   lat.rda.summary = summary(lat.rda)
-  lat.constrained.var = lat.rda.summary$cont$importance[2, ]
-  lat.unconstrained.var = lat.rda.summary$cont$importance[3, ]
+  lat.constrained.var = lat.rda.summary$constr.chi/lat.rda.summary$tot.chi
+  lat.unconstrained.var = lat.rda.summary$unconst.chi/lat.rda.summary$tot.chi
   
   # significance of rda
   lat.anova = anova.cca(lat.rda)
-  lat_anova_p_value <- lat_anova$`Pr(>F)`[1]
+  lat_anova_p_value <- lat.anova$`Pr(>F)`[1]
   
   # rda with longitude, get adjusted R2, constrained and unconstrained variance
   long.rda = rda(response ~ decimalLongitude, species)
   long_r2 <- RsquareAdj(long.rda)$adj.r.squared
   long.rda.summary = summary(long.rda)
-  long.constrained.var = long.rda.summary$cont$importance[2, ]
-  long.unconstrained.var = long.rda.summary$cont$importance[3, ]
+  long.constrained.var = long.rda.summary$constr.chi/long.rda.summary$tot.chi
+  long.unconstrained.var = long.rda.summary$unconst.chi/long.rda.summary$tot.chi
   
   # significance of rda
   long.anova = anova.cca(long.rda)
-  long_anova_p_value <- long_anova$`Pr(>F)`[1]
+  long_anova_p_value <- long.anova$`Pr(>F)`[1]
   
   # Create a row with all the results for the current species
-  species_name <- gsub(".csv", "", basename(species_file))  # Extract species name from file name
+  species_name <- gsub("^clim_data_|\\.csv$", "", basename(species_file)) %>%
+    gsub(" ", "_", .)  # Extract species name from file name
   
   species_results <- data.frame(
     species = species_name,
+    n = number.individuals,
     full_r2 = full_r2,
-    full_constrained_var = full_constrained_var,
-    full_unconstrained_var = full_unconstrained_var,
-    geo_cor = geo_cor,
+    full_constrained_var = full.constrained.var,
+    full_unconstrained_var = full.unconstrained.var,
+    geo_cor = geo.cor,
     lat_r2 = lat_r2,
-    lat_constrained_var = lat_constrained_var,
-    lat_unconstrained_var = lat_unconstrained_var,
+    lat_constrained_var = lat.constrained.var,
+    lat_unconstrained_var = lat.unconstrained.var,
     long_r2 = long_r2,
-    long_constrained_var = long_constrained_var,
-    long_unconstrained_var = long_unconstrained_var,
+    long_constrained_var = long.constrained.var,
+    long_unconstrained_var = long.unconstrained.var,
     full_anova_p_value = full_anova_p_value,
+    full_anova_lat_p_value = full_anova_lat_p_value,
+    full_anova_long_p_value = full_anova_long_p_value,
     lat_anova_p_value = lat_anova_p_value,
     long_anova_p_value = long_anova_p_value,
     stringsAsFactors = FALSE
@@ -103,34 +123,79 @@ for(species_file in species.list){
   results <- rbind(results, species_results)
 }
 
+write.csv(results, file = "./Results/Q1.RDA.results.csv")
+# remove species that didn't have at least 100 individuals
+
+results.2 = results %>%
+  filter(n > 99)
+
+# number of rows where geo.cor > 0.5
+results.2 %>%
+  filter(geo_cor > 0.5 | geo_cor < -0.5) %>%
+  nrow()
+# 40 out of 125
+
+range(results.2$full_r2)
+range(results.2$full_constrained_var)
+quantile(results.2$full_constrained_var)
+#0%       25%       50%       75%      100% 
+#0.1373631 0.4387693 0.5442747 0.6039492 0.7930756 
+
+# number of species with less than half variance explained
+results.2 %>%
+  filter(full_constrained_var < 0.50) %>%
+  nrow()
+# 55 out of 125 (44%)
+
+# number of species with less than 30% variance explained
+results.2 %>%
+  filter(full_constrained_var < 0.30) %>%
+  nrow()
+# 13 out of 125 (10%)
+
+range(results.2$lat_constrained_var)
+range(results.2$long_constrained_var)
+
+# number of species where latitude explains > 50% of variation
+results.2 %>%
+  filter(lat_constrained_var > 0.50) %>%
+  nrow()
+# 14 out of 125 (11%)
+
+# number of species where longitude explains > 50% of variation
+results.2 %>%
+  filter(long_constrained_var > 0.50) %>%
+  nrow()
+# 2 out of 125 (1.6%)
+
+# subset to models with lower geo.cor
+results.3 = results.2 %>%
+  filter(geo_cor < 0.5 & geo_cor > -0.5)
+# 85 species
+
+# number of species with less than half variance explained
+results.3 %>%
+  filter(full_constrained_var < 0.50) %>%
+  nrow()
+# 35 out of 85 (41%)
+
+# number of species with less than 30% variance explained
+results.3 %>%
+  filter(full_constrained_var < 0.30) %>%
+  nrow()
+# 4 out of 85 (5%)
+
+# number of species where latitude explains > 50% of variation
+results.3 %>%
+  filter(lat_constrained_var > 0.50) %>%
+  nrow()
+# 5 out of 85 (6%)
+
+# number of species where longitude explains > 50% of variation
+results.3 %>%
+  filter(long_constrained_var > 0.50) %>%
+  nrow()
+# 0 out of 85 (0%)
 
 
-
-
-A.fraseri.rda = rda(response.2 ~ decimalLatitude + decimalLongitude, A.fraseri)
-full_r2 <- RsquareAdj(A.fraseri.rda)$adj.r.squared # 0.13
-summary(A.fraseri.rda)
-# constrained proportion: variance of Y explained by X
-# unconstrained proportion: unexplained variance in Y
-
-vif.cca(A.fraseri.rda) # need to be below 3
-
-# significance testing using anova.cca, automatically does 999 permutations.
-# F statistic correspons to an overall test of sign. of an RDA by comparing the computed model to
-# a null model. This test is based on the null hypothesis that the strength of the linear relationship
-# calculated by the R2 is not larger than the value that would be obtained for unrelated Y and X matrices
-# of the same size. 
-anova.cca(A.fraseri.rda)
-anova.cca(A.fraseri.rda, by = "terms")
-
-ordiplot(A.fraseri.rda, scaling = 1, type = "text")
-ordiplot(A.fraseri.rda, scaling = 2, type = "text")
-
-A.fraseri.rda.lat = rda(response.2 ~ decimalLatitude, A.fraseri)
-RsquareAdj(A.fraseri.rda.lat)$adj.r.squared
-A.fraseri.rda.long = rda(response.2 ~ decimalLongitude, A.fraseri)
-RsquareAdj(A.fraseri.rda.long)$adj.r.squared
-anova.cca(A.fraseri.rda.long)
-
-test = as.data.frame(unique(clim.data$species))
 
